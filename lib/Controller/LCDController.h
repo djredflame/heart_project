ebb46109729860b406c35053b9f627eadf82bda5
Fatlum Epiroti <fatlumepiroti@Dj-Redflames-MacBook-Pro.local>
2024-12-27 13:10:51 +0100

    feat: changed header to just header

--- lib/Controller/LCDController.h
+++ lib/Controller/LCDController.h
@@ -1,4 +1,3 @@
-// LCDController.h
 #ifndef LCDCONTROLLER_H
 #define LCDCONTROLLER_H
 
@@ -10,32 +9,11 @@ private:
     LiquidCrystal lcd;
 
 public:
-    LCDController(int rs, int en, int d4, int d5, int d6, int d7)
-        : lcd(rs, en, d4, d5, d6, d7) {}
-
-    void init()
-    {
-        lcd.begin(16, 2);
-        lcd.clear();
-    }
-
-    void clear()
-    {
-        lcd.clear();
-    }
-
-    void print(const String &message, int line = 0, int col = 0)
-    {
-        lcd.setCursor(col, line);
-        lcd.print(message);
-    }
-
-    void update(const String &line1, const String &line2 = "")
-    {
-        clear();
-        print(line1, 0, 0);
-        print(line2, 1, 0);
-    }
+    LCDController(int rs, int en, int d4, int d5, int d6, int d7);
+    void init();
+    void clear();
+    void print(const String &message, int line = 0, int col = 0);
+    void update(const String &line1, const String &line2 = "");
 };
 
 #endif
